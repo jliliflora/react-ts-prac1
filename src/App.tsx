32d845bed8977e7ccdcfbdea6b5b271cc8974620
jliliflora <syon704@gmail.com>
2023-05-30 20:04:33 +0900

    Prac TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [value, setValue] = useState("");
+
+  //event에 타입 지정해주었음
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value);
+    // event를 열어서 currentTarget에 있는 value값 꺼내오기
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // ??
+    console.log("hello", value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
