5fc4b3eea8c3d009c87c4fd49fefb1b62630afa9
jliliflora <syon704@gmail.com>
2023-05-30 19:46:14 +0900

    Prac TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import Circle from "./Circle";
-
 function App() {
-  return (
-    <div>
-      <Circle borderColor="blue" bgColor="tomato" />
-      <Circle text="I'm here" bgColor="teal" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/Prac-TS/App-circle.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function AppCircle() {
+  return (
+    <div>
+      <Circle borderColor="blue" bgColor="tomato" />
+      <Circle text="I'm here" bgColor="teal" />
+    </div>
+  );
+}
+
+export default AppCircle;
