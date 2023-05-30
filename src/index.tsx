ea2fa9c116ab47c9154ecb2c3a0962f7f1bb0eb7
jliliflora <syon704@gmail.com>
2023-05-30 18:07:09 +0900

    install TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,9 @@ const lightTheme = {
   textColor: "#111",
   backgroundColor: "whitesmoke",
 };
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={lightTheme}>
